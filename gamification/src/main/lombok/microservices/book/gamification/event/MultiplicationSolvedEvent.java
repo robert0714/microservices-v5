c3977b29c0f87aa686a049a1ba21f8d2b8cba214
robert.lee <robert0714@gmail.com>
2018-04-16 16:47:26 +0800

    fixbug of ramittmq receiving

--- gamification/src/main/lombok/microservices/book/gamification/event/MultiplicationSolvedEvent.java
+++ gamification/src/main/lombok/microservices/book/gamification/event/MultiplicationSolvedEvent.java
@@ -3,22 +3,28 @@ package microservices.book.gamification.event;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.ToString;
-
+import com.fasterxml.jackson.annotation.JsonCreator;
 import java.io.Serializable;
 
 /**
  * Event received when a multiplication has been solved in the system.
  * Provides some context information about the multiplication.
  */
-@RequiredArgsConstructor
+//@AllArgsConstructor(onConstructor=@__(@JsonCreator))
+@AllArgsConstructor 
 @Getter
 @ToString
 @EqualsAndHashCode
-class MultiplicationSolvedEvent implements Serializable {
+public class MultiplicationSolvedEvent implements Serializable {
 
-    private final Long multiplicationResultAttemptId;
-    private final Long userId;
-    private final boolean correct;
-
+	private static final long serialVersionUID = 762582488774660776L;
+    private Long multiplicationResultAttemptId;
+    private Long userId;
+    private boolean correct;
+    
+    public   MultiplicationSolvedEvent() {
+    	super();
+    }
 }
